092ae568d022ba93fbdc87928b0166c802f09fcb
Chris Luedtke <chrisluedtke@gmail.com>
2018-11-25 13:16:14 -0600

    styling

--- busy_beaver/userstats/script.py
+++ busy_beaver/userstats/script.py
@@ -9,6 +9,7 @@ github = GitHubAdapter(oauth_token)
 
 boundary_dt = subtract_timedelta(timedelta(days=1))
 
+
 def recent_activity_text(user):
     activity = github.user_activity_after(user, boundary_dt)
 
@@ -25,15 +26,16 @@ def recent_activity_text(user):
             events_of_interest.append(event_of_interest)
 
     if len(events_of_interest) > 1:
-        text = f"*{user}*\n"
+        text = f"<@{user}> as <https://github.com/GitUser|GitUser>\n"
         for event_type in sorted(list(set([e['type'] for e in events_of_interest]))):
-            events = [event for event in events_of_interest if event['type']==event_type]
+            events = [event for event in events_of_interest if event['type'] == event_type]
             repos = list(set([event['repo'] for event in events]))
             repo_count = len(repos)
 
             if event_type == 'PushEvent':
                 commit_count = sum([event['commit_count'] for event in events])
-                text += f"* pushed {commit_count} commits to {repo_count} repo(s): {', '.join(repos)}\n"
+                text += f"* pushed {commit_count} commits to " +
+                        f"{repo_count} repo(s): {', '.join(repos)}\n"
             if event_type == 'WatchEvent':
                 text += f"* starred {repo_count} repo(s): {', '.join(repos)}\n"
     else:
