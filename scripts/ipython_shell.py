29db5cafc5fb076ddfd44868deb01ad4a084c96a
Chris <chrisluedtke@gmail.com>
2018-11-25 13:24:39 -0600

    Calculate user stats (#6)

--- scripts/ipython_shell.py
+++ scripts/ipython_shell.py
@@ -5,6 +5,7 @@ from IPython import embed
 
 from busy_beaver.adapters.github import GitHubAdapter  # noqa
 from busy_beaver.adapters.utilities import subtract_timedelta  # noqa
+from busy_beaver.userstats.script import recent_activity_text  # noqa
 
 from busy_beaver import db  # noqa
 from busy_beaver.models import *  # noqa
