b1c8ba3a5c904e026704c13fa8954014dc854dfe
Chris Luedtke <chrisluedtke@gmail.com>
2018-11-25 12:18:38 -0600

    recent_activity_text() for one user

--- scripts/ipython_shell.py
+++ scripts/ipython_shell.py
@@ -5,6 +5,7 @@ from IPython import embed
 
 from busy_beaver.adapters.github import GitHubAdapter  # noqa
 from busy_beaver.adapters.utilities import subtract_timedelta  # noqa
+from busy_beaver.userstats.script import recent_activity_text  # noqa
 
 
 oauth_token = os.getenv("GITHUB_OAUTH_TOKEN")
