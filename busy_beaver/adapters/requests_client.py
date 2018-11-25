7959e605040060e2016a5879d05e1a5e5913f7a2
Aly Sivji <alysivji@gmail.com>
2018-11-24 22:14:59 -0600

    spin out requests into client plugin

--- busy_beaver/adapters/requests_client.py
+++ busy_beaver/adapters/requests_client.py
@@ -1,21 +1,36 @@
+from json import JSONDecodeError
+import logging
+from typing import Any, Dict, List, NamedTuple, Union
+
 import requests
 
-USER_AGENT = "BusyBeaver"
+logger = logging.getLogger(__name__)
+
+
+class Response(NamedTuple):
+    status_code: int
+    headers: Dict[str, str]
+    json: Union[List[Dict[str, Any]], Dict[str, Any]] = None
 
 
 class RequestsClient:
-    def __init__(self, oauth_token: str):
+    def __init__(self):
         s = requests.Session()
-        s.headers = {
-            "Accept": "application/vnd.github.v3+json",
-            "Authorization": f"token {oauth_token}",
-            "Content-Type": "application/json",
-            "User-Agent": USER_AGENT,
-        }
         self.session = s
 
     def __repr__(self):
         return "RequestsClient"
 
-    def request(self, method: str, url: str, **kwargs) -> requests.Response:
-        return self.session.request(method, url, **kwargs)
+    def _request(self, method: str, url: str, **kwargs) -> requests.Response:
+        r = self.session.request(method, url, **kwargs)
+        try:
+            resp = Response(status_code=r.status_code, headers=r.headers, json=r.json())
+        except JSONDecodeError:
+            resp = Response(status_code=r.status_code, headers=r.headers)
+        return resp
+
+    def get(self, url: str, **kwargs) -> Response:
+        return self._request("get", url, **kwargs)
+
+    def head(self, url: str, **kwargs) -> Response:
+        return self._request("head", url, **kwargs)
