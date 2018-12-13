29b79cf1f5300da505234198c9bd54c01a0d2a95
Aly Sivji <alysivji@gmail.com>
2018-12-12 23:26:20 -0600

    create dir for logs if it doesnt exist

--- busy_beaver/__init__.py
+++ busy_beaver/__init__.py
@@ -1,11 +1,14 @@
 import logging
 import logging.config
+import pathlib
 
 import responder
 import sentry_sdk
 from sqlalchemy_wrapper import SQLAlchemy
 
 from .config import DATABASE_URI, IN_PRODUCTION, LOGGING_CONFIG, SENTRY_DSN
+
+pathlib.Path('logs').mkdir(exist_ok=True)
 
 # observability
 logging.config.dictConfig(LOGGING_CONFIG)
