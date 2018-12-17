b1df26661b5ced538f6365d15d53ade1cff24472
Chris Luedtke <chrisluedtke@gmail.com>
2018-12-16 18:51:00 -0600

    `attachment` as dictionary

--- busy_beaver/backend.py
+++ busy_beaver/backend.py
@@ -1,4 +1,3 @@
-import json
 import logging
 import os
 import time
@@ -130,7 +129,7 @@ def reply_to_user_with_github_login_link(event):
         }
     ]
     slack.post_message(
-        channel=channel, text=ACCOUNT_ASSOCIATE_MSG, attachments=json.dumps(attachment)
+        channel=channel, text=ACCOUNT_ASSOCIATE_MSG, attachments=attachment)
     )
     return
 
