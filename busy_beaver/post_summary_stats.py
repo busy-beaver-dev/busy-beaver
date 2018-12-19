b7f6f92e6519b0cef14a678ecc35162822418547
Aly Sivji <alysivji@gmail.com>
2018-12-19 09:42:17 -0600

    filter null github ids and refactor

--- busy_beaver/post_summary_stats.py
+++ busy_beaver/post_summary_stats.py
@@ -1,14 +1,30 @@
 import os
 from typing import List
 
+from . import db, api
 from . import github_stats
 from .adapters.slack import SlackAdapter
-
-from . import db, api
 from .models import User
+
+from sqlalchemy import and_
 
 slack_token = os.getenv("SLACK_BOTUSER_OAUTH_TOKEN")
 slack = SlackAdapter(slack_token)
+
+
+@api.background.task
+def post_summary(channel: str) -> None:
+    channel_id = get_channel_id(channel)
+    members = get_channel_members(channel_id)
+
+    text = ""
+    users: List[User] = db.query(User).filter(
+        and_(User.slack_id.in_(members)), User.github_username.isnot(None)
+    ).all()
+    for user in users:
+        text += github_stats.recent_activity_text(user)
+
+    slack.post_message(channel_id, text)
 
 
 def get_channel_id(channel_name):
@@ -19,16 +35,3 @@ def get_channel_id(channel_name):
 def get_channel_members(channel_id: str) -> List[str]:
     channel_info = slack.get_channel_info(channel_id)
     return channel_info["channel"]["members"]
-
-
-@api.background.task
-def post_summary(channel: str) -> None:
-    channel_id = get_channel_id(channel)
-    members = get_channel_members(channel_id)
-
-    text = ""
-    users: List[User] = db.query(User).filter(User.slack_id.in_(members)).all()
-    for user in users:
-        text += github_stats.recent_activity_text(user)
-
-    slack.post_message(channel_id, text)
