286207d3545ad82ce2bdea7c3e1042ba8672e729
Aly Sivji <alysivji@gmail.com>
2019-01-31 12:11:35 -0600

    use auth decorator inside of functions

--- busy_beaver/backend/tasks.py
+++ busy_beaver/backend/tasks.py
@@ -1,41 +1,25 @@
 import logging
 
-from .. import db
+from .decorators import authentication_required
 from ..config import TWITTER_USERNAME
-from ..models import ApiUser
 from ..tasks import post_github_summary_to_slack
 from ..retweeter import post_tweets_to_slack
 
 logger = logging.getLogger(__name__)
-# TODO add authentication middleware
 
 
 class PublishGitHubSummaryResource:
     """Endpoint to trigger process of creating and publishing GitHub Summary to Slack
     """
 
-    async def on_post(self, req, resp):
-        logger.info("[Busy-Beaver] Post GitHub Summary Request")
-
-        if "authorization" not in req.headers:
-            logger.error("[Busy-Beaver] Post GitHub Summary Request -- no auth header")
-            resp.status_code = 401
-            resp.media = {"message": "Include header: Authorization: 'token {token}'"}
-            return
-
-        token = req.headers["authorization"].split("token ")[1]
-        api_user: ApiUser = db.query(ApiUser).filter_by(token=token).first()
-        if not api_user:
-            logger.error("[Busy-Beaver] Invalid token")
-            resp.status_code = 401
-            resp.media = {"message": "Invalid token, please talk to admin"}
-            return
-
-        # TODO maybe add a task queue here
+    @authentication_required
+    async def on_post(self, req, resp, user):
         logger.info(
             "[Busy-Beaver] Post GitHub Summary Request -- login successful",
-            extra={"api_user": api_user.username},
+            extra={"user": user.username},
         )
+
+        # TODO maybe add a task queue here
         data = await req.media()
         if "channel" not in data:
             logger.error(
@@ -53,28 +37,14 @@ class TwitterPollingResource:
     """Endpoint to trigger polling of Twitter for new tweets to post to channel
     """
 
-    async def on_post(self, req, resp):
-        logger.info("[Busy-Beaver] Twitter Summary Poll")
-
-        if "authorization" not in req.headers:
-            logger.error("[Busy-Beaver] Twitter Summary Poll -- no auth header")
-            resp.status_code = 401
-            resp.media = {"message": "Include header: Authorization: 'token {token}'"}
-            return
-
-        token = req.headers["authorization"].split("token ")[1]
-        api_user: ApiUser = db.query(ApiUser).filter_by(token=token).first()
-        if not api_user:
-            logger.error("[Busy-Beaver] Invalid token")
-            resp.status_code = 401
-            resp.media = {"message": "Invalid token, please talk to admin"}
-            return
-
-        # TODO maybe add a task queue here
+    @authentication_required
+    async def on_post(self, req, resp, user):
         logger.info(
             "[Busy-Beaver] Twitter Summary Poll -- login successful",
-            extra={"api_user": api_user.username},
+            extra={"user": user.username},
         )
+
+        # TODO maybe add a task queue here
         data = await req.media()
         if "channel" not in data:
             logger.error(
