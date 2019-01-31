ffe1250d0156f71d1053f37c8070ca0dd888348f
Aly Sivji <4369343+alysivji@users.noreply.github.com>
2019-01-31 12:19:25 -0600

    Authentication Decorator (#55)

--- busy_beaver/backend/sandbox.py
+++ busy_beaver/backend/sandbox.py
@@ -1,7 +1,7 @@
 import logging
 import time
-
 from .. import api
+from .decorators import authentication_required
 
 logger = logging.getLogger(__name__)
 
@@ -9,9 +9,17 @@ logger = logging.getLogger(__name__)
 class HelloWorldResource:
     """For testing purposes"""
 
-    def on_get(self, req, resp):
+    async def on_get(self, req, resp):
         logger.info("[Busy-Beaver] Hit hello world endpoint", extra={"test": "payload"})
         resp.media = {"Hello": "World"}
+
+
+class GoodbyeWorldResource:
+    """For testing purposes, part deux"""
+
+    @authentication_required
+    async def on_get(self, req, resp, user):
+        resp.media = {"See You": "Space Cowboy"}
 
 
 @api.background.task
