f29321fcf7227c60bb3a56a2eb04ad398fd92759
Aly Sivji <alysivji@gmail.com>
2019-03-03 09:44:11 -0600

    add authentication required decorator with authorized roles

--- busy_beaver/decorators.py
+++ busy_beaver/decorators.py
@@ -1,48 +1,51 @@
-from functools import wraps
 import logging
 import re
 
 from flask import request
 
-from .extensions import db
 from .models import ApiUser
+from .toolbox import make_response
 
 logger = logging.getLogger(__name__)
 AUTH_STRING = re.compile(r"token (?P<token>.*)")
 
 
 # TODO... get this sorted
-def authentication_required(func):
-    @wraps
-    def _token_auth(*args, **kwargs):
-        if "authorization" not in request.headers:
-            logger.error("[Busy-Beaver] No auth header")
-            # resp.status_code = 401
-            # resp.media = {"message": "Missing header: Authorization: 'token {tkn}'"}
-            return
+def authentication_required(roles):
+    """Requires request to Authentication header set to 'token {token goes here}'"""
+    if not isinstance(roles, list):
+        raise ValueError
 
-        m = AUTH_STRING.match(request.headers["authorization"])
-        if not m:
-            # resp.status_code = 401
-            # resp.media = {"message": "Expected header: Authenication: 'token {tkn}'"}
-            return
+    def auth_decorator(func):
+        def _token_auth(*args, **kwargs):
+            if "authorization" not in request.headers:
+                logger.error("[Busy-Beaver] No auth header")
+                data = {"message": "Missing header: Authorization: 'token {token}'"}
+                return make_response(401, error=data)
 
-        token = m.group("token")
-        api_user: ApiUser = db.query(ApiUser).filter_by(token=token).first()
-        if not api_user:
-            logger.error("[Busy-Beaver] Invalid token")
-            # resp.status_code = 401
-            # resp.media = {"message": "Invalid token, contact admin"}
-            return
+            m = AUTH_STRING.match(request.headers["authorization"])
+            if not m:
+                data = {"message": "Expected header: Authorization: 'token {token}'"}
+                return make_response(401, error=data)
 
-        # TODO enchancement
-        # if api_user.role not in roles:
-        #     logger.error("[Busy-Beaver] Unauthorized access of endpoint")
-        #     # resp.status_code = 401
-        #     # resp.media = {"message": "Not authorized to access endpoint, contact to admin"}
-        #     return
+            token = m.group("token")
+            api_user: ApiUser = ApiUser.query.filter_by(token=token).first()
+            if not api_user:
+                logger.error("[Busy-Beaver] Invalid token")
+                data = {"message": "Invalid token, contact admin"}
+                return make_response(401, error=data)
 
-        # TODO put user in request context
-        # , user=api_user
-        return func(*args, **kwargs)
-    return _token_auth
+            # TODO enchancement
+            if api_user.role not in roles:
+                logger.error("[Busy-Beaver] Unauthorized access of endpoint")
+                data = {"message": "Not authorized to access endpoint, contact to admin"}
+                return make_response(401, error=data)
+
+            # TODO put user in request context
+            # , user=api_user
+            return func(*args, **kwargs)
+        # Not sure why we need this line... can funtools fix it?
+        # https://stackoverflow.com/questions/17256602/assertionerror-view-function-mapping-is-overwriting-an-existing-endpoint-functi
+        _token_auth.__name__ = func.__name__
+        return _token_auth
+    return auth_decorator
--- busy_beaver/toolbox.py
+++ busy_beaver/toolbox.py
@@ -1,5 +1,5 @@
 from datetime import datetime, timedelta
-import json
+import json as _json
 
 from flask import Response
 import pytz
@@ -10,12 +10,12 @@ def utc_now_minus(period: timedelta):
 
 
 def make_response(
-    status_code: int = 200, *, headers: dict = {}, data: dict = {}, error: dict = {}
+    status_code: int = 200, *, headers: dict = {}, json: dict = {}, error: dict = {}
 ):
     """Build and send response"""
     resp = {"data": None, "error": None}
-    if data:
-        resp["data"] = data
+    if json:
+        resp["data"] = json
     if error:
         resp["error"] = error
 
@@ -23,23 +23,5 @@ def make_response(
         status=status_code,
         headers=headers,
         content_type="application/json",
-        response=json.dumps(resp),
+        response=_json.dumps(resp),
     )
-
-
-# def deserialize_request(schema, data):
-#     """Wrapper for Marhsmallow's schema.loads"""
-#     try:
-#         deserialized_result = schema.load(data)
-#     except ValidationError as exc:
-#         raise DeserializationError(payload=exc.messages)
-#     return deserialized_result
-
-
-# def serialize_response(schema, data):
-#     """Wrapper for Marhsmallow's schema.dumps"""
-#     try:
-#         serialized_result = schema.dumps(data)
-#     except ValidationError as exc:
-#         raise SerializationError(payload=exc.messages)
-#     return serialized_result
