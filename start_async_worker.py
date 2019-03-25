f366159f7fdafc6e2c822a36c839a1a06d2ce288
Aly Sivji <alysivji@gmail.com>
2019-03-25 18:28:52 -0500

    add retry failed job handler

--- start_async_worker.py
+++ start_async_worker.py
@@ -6,6 +6,7 @@ from sentry_sdk.integrations.rq import RqIntegration
 from busy_beaver.app import create_app
 from busy_beaver.config import IN_PRODUCTION, LOGGING_CONFIG, SENTRY_DSN
 from busy_beaver.extensions import rq
+from busy_beaver.tasks.handlers import retry_failed_job
 
 # observability
 LOGGING_CONFIG["handlers"]["datadog_file"]["filename"] = "logs/busy_beaver_worker.json"
@@ -18,4 +19,5 @@ ctx = app.app_context()
 ctx.push()
 
 w = rq.get_worker("default", "failed")
+w.push_exc_handler(retry_failed_job)
 w.work()
