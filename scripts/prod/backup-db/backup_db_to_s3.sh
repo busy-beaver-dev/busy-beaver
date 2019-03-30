1d4394b7a00c7ba8d814ee222a2bfba0cefd8351
Aly Sivji <4369343+alysivji@users.noreply.github.com>
2019-03-30 09:05:10 -0500

    Update requirements (#106)

--- scripts/prod/backup-db/backup_db_to_s3.sh
+++ scripts/prod/backup-db/backup_db_to_s3.sh
@@ -4,10 +4,10 @@ source ${HOME}/.bash_profile
 
 docker-compose \
     -f ${HOME}/busy-beaver/docker-compose.prod.yml \
-    exec db pg_dump -U ${POSTGRES_USER} busy-beaver > /tmp/data_dump.sql
+    exec db pg_dump -U ${POSTGRES_USER} busy-beaver > ${HOME}/data_dump.sql
 
 docker run --rm -t \
     -v ${HOME}/.aws:/home/worker/.aws:ro \
     -v ${HOME}/busy-beaver/scripts/prod/backup-db:/work \
-    -v /tmp/data_dump.sql:/tmp/data_dump.sql \
+    -v ${HOME}/data_dump.sql:/tmp/data_dump.sql \
     shinofara/docker-boto3 python /work/upload_db_backup_to_s3.py
