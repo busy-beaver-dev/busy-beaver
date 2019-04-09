1f1e00e829243344d78c1d77614fc9f40051cf15
Aly Sivji <alysivji@gmail.com>
2019-04-09 14:12:01 -0500

    entrypoint script for worker and server

---
+++ scripts/entrypoint.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+if [ "$1" = "webserver" ]; then
+    echo "Starting Flask server"
+    flask db upgrade
+    if [ "$IN_PRODUCTION" = 1 ]; then
+        gunicorn "busy_beaver:create_app()" -b 0.0.0.0:5000
+    else
+        gunicorn "busy_beaver:create_app()" -b 0.0.0.0:5000 --reload --timeout 100000
+    fi
+elif [ "$1" = "worker" ]; then
+    echo "Starting RQ worker"
+    python start_async_worker.py
+else
+    exit 1
+fi
+
+exec $CMD "$@"
