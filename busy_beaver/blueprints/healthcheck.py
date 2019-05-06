80bd4c97e35aa9471824f211209957ca24d0187a
Matt Layman <matthewlayman@gmail.com>
2019-05-06 10:45:33 -0400

    Add Black to Travis CI (#127)

--- busy_beaver/blueprints/healthcheck.py
+++ busy_beaver/blueprints/healthcheck.py
@@ -1,9 +1,9 @@
 from flask import jsonify
 from flask import blueprints
 
-healthcheck_bp = blueprints.Blueprint('healthcheck', __name__)
+healthcheck_bp = blueprints.Blueprint("healthcheck", __name__)
 
 
-@healthcheck_bp.route('/healthcheck', methods=['GET'])
+@healthcheck_bp.route("/healthcheck", methods=["GET"])
 def health_check():
     return jsonify({"ping": "pong"})
