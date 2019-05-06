6498df81eab57284f2a2445465d84fe9c3d3a21a
Matt Layman <matthewlayman@gmail.com>
2019-05-06 10:42:09 -0400

    Blackify some missing files.

--- busy_beaver/adapters/__init__.py
+++ busy_beaver/adapters/__init__.py
@@ -1,5 +1,5 @@
 from .github import GitHubAdapter  # noqa
 from .key_value_store import KeyValueStoreAdapter  # noqa
 from .requests_client import RequestsClient  # noqa
-from .slack import SlackAdapter   # noqa
+from .slack import SlackAdapter  # noqa
 from .twitter import TwitterAdapter  # noqa
