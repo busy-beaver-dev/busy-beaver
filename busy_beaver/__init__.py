fd67c7c2be04a6c788baec04dff8e3391ce49e1a
Cris Ewing <cris@crisewing.com>
2019-05-07 14:13:06 -0400

    convert content of init file to use config class

--- busy_beaver/__init__.py
+++ busy_beaver/__init__.py
@@ -5,37 +5,27 @@ import sentry_sdk
 from sentry_sdk.integrations.flask import FlaskIntegration
 
 from .adapters import GitHubAdapter, KeyValueStoreAdapter, SlackAdapter, TwitterAdapter
-from .config import (
-    GITHUB_OAUTH_TOKEN,
-    IN_PRODUCTION,
-    LOGGING_CONFIG,
-    SENTRY_DSN,
-    SLACK_TOKEN,
-    TWITTER_ACCESS_TOKEN,
-    TWITTER_ACCESS_TOKEN_SECRET,
-    TWITTER_CONSUMER_KEY,
-    TWITTER_CONSUMER_SECRET,
-)
+from .config import IN_PRODUCTION, config
 
 # Forgot why I do this... probalby should have commented as it is not ovious
 # TODO test if it is needed in datadog
 pathlib.Path("logs").mkdir(exist_ok=True)
 
 # observability
-load_dict_config(LOGGING_CONFIG)
+load_dict_config(config.LOGGING_CONFIG)
 logger = logging.getLogger(__name__)
-if IN_PRODUCTION and SENTRY_DSN:
-    sentry_sdk.init(dsn=SENTRY_DSN, integrations=[FlaskIntegration()])
+if IN_PRODUCTION and config.SENTRY_DSN:
+    sentry_sdk.init(dsn=config.SENTRY_DSN, integrations=[FlaskIntegration()])
 
 logger.info("[BusyBeaver] Configure Integrations")
-github = GitHubAdapter(GITHUB_OAUTH_TOKEN)
+github = GitHubAdapter(config.GITHUB_OAUTH_TOKEN)
 kv_store = KeyValueStoreAdapter()
-slack = SlackAdapter(SLACK_TOKEN)
+slack = SlackAdapter(config.SLACK_TOKEN)
 twitter = TwitterAdapter(
-    TWITTER_CONSUMER_KEY,
-    TWITTER_CONSUMER_SECRET,
-    TWITTER_ACCESS_TOKEN,
-    TWITTER_ACCESS_TOKEN_SECRET,
+    config.TWITTER_CONSUMER_KEY,
+    config.TWITTER_CONSUMER_SECRET,
+    config.TWITTER_ACCESS_TOKEN,
+    config.TWITTER_ACCESS_TOKEN_SECRET,
 )
 
 logger.info("[BusyBeaver] Starting Server")
