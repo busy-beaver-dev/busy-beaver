b198264652eb6f2fcc3732613ddfb3e3790cbc19
Aly Sivji <alysivji@gmail.com>
2019-06-07 08:40:49 -0500

    setup framework for event emitter

--- busy_beaver/blueprints/github/event_subscription.py
+++ busy_beaver/blueprints/github/event_subscription.py
@@ -3,7 +3,11 @@ import logging
 from flask import jsonify, request
 from flask.views import MethodView
 
+from busy_beaver.exceptions import UnverifiedWebhookRequest
+from busy_beaver.toolbox import EventEmitter
+
 logger = logging.getLogger(__name__)
+github_event_dispatcher = EventEmitter()
 
 
 class GitHubEventSubscriptionResource(MethodView):
@@ -13,5 +17,24 @@ class GitHubEventSubscriptionResource(MethodView):
         data = request.json
         logger.info("[Busy Beaver] Received GitHub event", extra={"req_json": data})
 
-        # TODO eventemitter on header... X-GitHub-Event: ping
+        event_type = request.headers.get("X-GitHub-Event", None)
+        if not event_type:
+            return UnverifiedWebhookRequest("Missing GitHub event type")
+
+        github_event_dispatcher.emit(event_type, default="ping", data=data)
         return jsonify(True)
+
+
+@github_event_dispatcher.on("ping")
+def handle_ping(data):
+    pass
+
+
+@github_event_dispatcher.on("pr")  # TODO
+def handle_pr(data):
+    pass
+
+
+@github_event_dispatcher.on("issue")  # TODO
+def handle_issue(data):
+    pass
