455a5839bc2516d34e88032064756b302013cdfa
Aly Sivji <alysivji@gmail.com>
2019-06-08 17:34:32 -0500

    get migration working

--- migrations/versions/20190526_22-47-39__change_column_name_and_add_end_epoch_in_.py
+++ migrations/versions/20190526_22-47-39__change_column_name_and_add_end_epoch_in_.py
@@ -17,13 +17,23 @@ depends_on = None
 
 
 def upgrade():
+    # Step 1: Schema table
+    # Change column name (to start_epoch) and add end_epoch column
     op.alter_column(
         "event", "utc_epoch", new_column_name="start_epoch", server_default=None
     )
     op.add_column("event", sa.Column("end_epoch", sa.Integer(), nullable=True))
 
-    # copy start_epoch to end_epoch
-    # alter column to nullable = False
+    # Step 2: Data Migration
+    # Set end_epoch col to start_epoch col
+    engine = op.get_bind()
+    meta = sa.MetaData(bind=engine)
+    event = sa.Table("event", meta, autoload=True)
+    stmt = event.update().values(end_epoch=event.c.start_epoch)
+    engine.execute(stmt)
+
+    # Step 3: Schema table
+    # end_epoch column can't be empty
     op.alter_column("event", "end_epoch", nullable=False)
 
 
