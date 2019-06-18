834f99c316e58788efbb3f1d4e501e7c9b7df323
Aly Sivji <alysivji@gmail.com>
2019-06-18 09:06:16 -0500

    add oauthflow quote-unquote interface

--- busy_beaver/apps/external_integrations/oauth_providers/base.py
+++ busy_beaver/apps/external_integrations/oauth_providers/base.py
@@ -8,3 +8,18 @@ class OAuthError(Exception):
 class ExternalOAuthDetails(NamedTuple):
     url: str
     state: str
+
+
+class OAuthFlowInterface:
+    """Defining a common API to add consistency to software design process"""
+
+    def __init__(self, client_id, client_secret):
+        return NotImplementedError
+
+    def generate_authentication_tuple(self) -> ExternalOAuthDetails:
+        """Creates a (URL, state) tuple used to authenticate users"""
+        return NotImplementedError
+
+    def process_callback(self, authorization_response_url, state):
+        """Handles callback made by authentication service servers to verify users"""
+        return NotImplementedError
