c0373dfac8385f99ea462a1739ee6c3a67bf96fc
Aly Sivji <alysivji@gmail.com>
2019-06-18 19:12:52 -0500

    get oauth2 flow working

--- busy_beaver/apps/external_integrations/workflow.py
+++ busy_beaver/apps/external_integrations/workflow.py
@@ -1,17 +1,31 @@
+from .oauth_providers.base import OAuthError
 from busy_beaver import slack_oauth
+from busy_beaver.extensions import db
+from busy_beaver.models import SlackInstallation
 
 
 def slack_generate_and_save_auth_tuple():
     auth = slack_oauth.generate_authentication_tuple()
-    # TODO save auth.state somewhere
+    new_installation = SlackInstallation(state=auth.state)
+    db.session.add(new_installation)
+    db.session.commit()
     return auth
-    # jsonify({"url": auth.url, "state": auth.state})
 
 
 def slack_verify_callback_and_save_access_tokens_in_database(callback_url, state):
-    state  # pull user from this
+    installation = SlackInstallation.query.filter_by(state=state).first()
+
+    if not installation:
+        raise OAuthError("state not found")
     oauth_details = slack_oauth.process_callback(callback_url, state)
 
-    # TODO pull user from state
-    # patch user with oauth_detail
-    print(oauth_details)
+    # does this exist?
+    workspace_installation = SlackInstallation.query.filter_by(
+        workspace_id=oauth_details.workspace_id
+    ).first()
+    if workspace_installation:
+        installation = workspace_installation
+
+    installation.patch(oauth_details._asdict().update({"state": ""}))
+    db.session.add(installation)
+    db.session.commit()
