35e730705904c21ee6ef5db81d9dbb791472451a
Aly Sivji <alysivji@gmail.com>
2019-06-23 19:12:22 -0500

    fix bash script

--- scripts/prod/run_github_summary_report.sh
+++ scripts/prod/run_github_summary_report.sh
@@ -6,4 +6,4 @@ curl https://busybeaver.sivji.com/poll/github-summary \
     -X POST \
     -H "Content-Type: application/json" \
     -H "Authorization: token ${BUSY_BEAVER_API_TOKEN}" \
-    -d '{"workspace_id": "T5G0FCMNW", "channel": "busy-beaver"}'
+    -d '{"workspace_id": "T093FC1RC", "channel": "busy-beaver"}'
