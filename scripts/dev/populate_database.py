405ce96d0eca4634e0b4cc0be1f6d6c08def9851
Aly Sivji <alysivji@gmail.com>
2019-06-30 16:57:21 -0500

    clean up populate db step

--- scripts/dev/populate_database.py
+++ scripts/dev/populate_database.py
@@ -7,6 +7,8 @@ from busy_beaver.models import SlackInstallation
 # load config
 workspace_id = os.getenv("SLACK_DEV_WORKSPACE_ID", None)
 bot_token = os.getenv("SLACK_BOTUSER_OAUTH_TOKEN", None)
+authorizing_user_id = os.getenv("SLACK_DEV_AUTHORIZING_USER_ID", "U5FTQ3QRZ")
+bot_user_id = os.getenv("SLACK_DEV_BOT_USER_ID", "UEGGXQ5EF")
 if not bot_token or not workspace_id:
     exit
 
@@ -18,17 +20,24 @@ ctx.push()
 # create or update record
 installation = SlackInstallation.query.filter_by(workspace_id=workspace_id).first()
 if installation:
-    installation.patch(data={"bot_access_token": bot_token})
+    installation.patch(
+        data={
+            "authorizing_user_id": authorizing_user_id,
+            "bot_access_token": bot_token,
+            "bot_user_id": bot_user_id,
+            "workspace_id": workspace_id,
+        }
+    )
 else:
     installation = SlackInstallation(
         access_token="access_token",
-        authorizing_user_id="U5FTQ3QRZ",  # TODO: make env variable or have people
+        authorizing_user_id=authorizing_user_id,
         bot_access_token=bot_token,
-        bot_user_id="UEGGXQ5EF",  # TODO: install this to get information for database
+        bot_user_id=bot_user_id,
         scope="test-scope",
         workspace_id=workspace_id,
         workspace_name="Development Environment",
-        state="installed",
+        state="active",
     )
 
 db.session.add(installation)
