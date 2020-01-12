eca32e5f43d39ce49fc5afe05b694321a904bd19
Aly Sivji <alysivji@gmail.com>
2020-01-12 13:18:39 -0600

    add logging to healthcheck endpoing

--- busy_beaver/blueprints/healthcheck.py
+++ busy_beaver/blueprints/healthcheck.py
@@ -1,9 +1,12 @@
+import logging
 from flask import jsonify
 from flask import blueprints
 
+logger = logging.getLogger(__name__)
 healthcheck_bp = blueprints.Blueprint("healthcheck", __name__)
 
 
 @healthcheck_bp.route("/healthcheck", methods=["GET"])
 def health_check():
+    logger.info("Hit healthcheck")
     return jsonify({"ping": "pong"})
