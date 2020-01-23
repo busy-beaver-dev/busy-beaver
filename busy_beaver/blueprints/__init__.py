1dbc8d7efa89509fc64772155cc8de3b892ce0b9
Aly Sivji <alysivji@gmail.com>
2020-01-23 14:07:40 -0600

    create debug app

--- busy_beaver/blueprints/__init__.py
+++ busy_beaver/blueprints/__init__.py
@@ -1,4 +1,4 @@
-from .healthcheck import healthcheck_bp  # noqa
+from busy_beaver.apps.debug.api.healthcheck import healthcheck_bp  # noqa
 from busy_beaver.apps.github_integration.api import github_bp  # noqa
 from busy_beaver.apps.poller.api import poller_bp  # noqa
 from busy_beaver.apps.slack_integration.api import slack_bp  # noqa
