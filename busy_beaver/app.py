87f9f9ca83fc5a1ae8d723d0e2eddc228b890615
Aly Sivji <alysivji@gmail.com>
2020-04-08 22:48:40 -0500

    update queue name

--- busy_beaver/app.py
+++ busy_beaver/app.py
@@ -40,7 +40,7 @@ def create_app(*, testing=False):
     migrate.init_app(app, db)
 
     app.config["RQ_REDIS_URL"] = REDIS_URI
-    app.config["RQ_QUEUES"] = ["default", "failed"]
+    app.config["RQ_QUEUES"] = ["busybeaver_default"]
     rq.init_app(app)
 
     talisman.init_app(app)
