aa974e96488ff5a96f10ce84565b154fe6a3b5a6
Aly Sivji <alysivji@gmail.com>
2020-04-09 16:50:12 -0500

    movie github blueprint to file

---
+++ busy_beaver/apps/github_integration/blueprint.py
@@ -0,0 +1,16 @@
+from flask import blueprints
+
+from .api.event_subscription import GitHubEventSubscriptionResource
+from .api.oauth import GitHubIdentityVerificationCallbackResource
+
+github_bp = blueprints.Blueprint("github", __name__)
+
+github_bp.add_url_rule(
+    "/event-subscription",
+    view_func=GitHubEventSubscriptionResource.as_view("github_event_subscription"),
+)
+
+github_bp.add_url_rule(
+    "/oauth",
+    view_func=GitHubIdentityVerificationCallbackResource.as_view("github_verification"),
+)
