f71149d8a87863b1b2532ab226c560277d1ea94a
Aly Sivji <4369343+alysivji@users.noreply.github.com>
2020-04-08 19:15:33 -0500

    Do not require HTTPS on healthcheck endpoint (#235)

--- busy_beaver/apps/debug/api/healthcheck.py
+++ busy_beaver/apps/debug/api/healthcheck.py
@@ -2,11 +2,14 @@ import logging
 
 from flask import blueprints, jsonify
 
+from busy_beaver.extensions import talisman
+
 logger = logging.getLogger(__name__)
 healthcheck_bp = blueprints.Blueprint("healthcheck", __name__)
 
 
 @healthcheck_bp.route("/healthcheck", methods=["GET"])
+@talisman(force_https=False)
 def health_check():
     logger.info("Hit healthcheck")
     return jsonify({"ping": "pong"})
