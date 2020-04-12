39859c92c852c7bc93416bbca3b7acae2c2be5b6
Aly Sivji <alysivji@gmail.com>
2020-04-12 11:20:25 -0500

    add workspace_id field

--- busy_beaver/models/key_value.py
+++ busy_beaver/models/key_value.py
@@ -10,9 +10,19 @@ class KeyValueStore(BaseModel):
     __tablename__ = "key_value_store"
 
     def __repr__(self):  # pragma: no cover
-        return f"<KeyValueStore: {self.key} {self.value}>"
+        workspace_name = {self.slack_installation.workspace_name}
+        return f"<KeyValueStore: {workspace_name} {self.key} {self.value}>"
 
     # Attributes
-    # TODO add slack workspace id once when we start being more than single tenant
+    installation_id = db.Column(
+        db.Integer,
+        db.ForeignKey("slack_installation.id", name="fk_installation_id"),
+        nullable=True,
+    )
     key = db.Column(db.String(255), nullable=False, unique=True)
     value = db.Column(db.LargeBinary(), nullable=False)
+
+    # Relationships
+    slack_installation = db.relationship(
+        "SlackInstallation", back_populates="key_value_pairs"
+    )
