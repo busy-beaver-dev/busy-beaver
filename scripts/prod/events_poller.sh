a84c5cb1a3b1d21868e3296d097c2e83c73f4904
Aly Sivji <4369343+alysivji@users.noreply.github.com>
2020-04-15 18:29:31 -0500

    Remove Ansible (#249)

--- scripts/prod/events_poller.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-source $HOME/.bash_profile
-
-curl https://busybeaver.sivji.com/poll/sync-event-database \
-    -X POST \
-    -H "Content-Type: application/json" \
-    -H "Authorization: token ${BUSY_BEAVER_API_TOKEN}"
