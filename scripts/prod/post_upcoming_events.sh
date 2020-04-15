6892c2c2d12c6ee7276809e0cfe994383de27c9b
Aly Sivji <alysivji@gmail.com>
2020-04-15 18:16:39 -0500

    removing polling scripts

--- scripts/prod/post_upcoming_events.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-source $HOME/.bash_profile
-
-curl https://busybeaver.sivji.com/poll/upcoming-events \
-    -X POST \
-    -H "Content-Type: application/json" \
-    -H "Authorization: token ${BUSY_BEAVER_API_TOKEN}" \
-    -d '{"channel": "announcements"}'
--- scripts/prod/run_github_summary_chipy.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-source $HOME/.bash_profile
-
-curl https://busybeaver.sivji.com/poll/github-summary \
-    -X POST \
-    -H "Content-Type: application/json" \
-    -H "Authorization: token ${BUSY_BEAVER_API_TOKEN}" \
-    -d '{"workspace_id": "T093FC1RC"}'
