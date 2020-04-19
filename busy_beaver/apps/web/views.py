01baf02ccd5184e4bca4f968b95485604c42bd32
Aly Sivji <alysivji@gmail.com>
2020-04-19 14:54:27 -0500

    get home page to show template

--- busy_beaver/apps/web/views.py
+++ busy_beaver/apps/web/views.py
@@ -1,17 +1,27 @@
 import logging
 
-from flask import jsonify
-from flask.views import MethodView
+from flask import render_template
+from flask.views import View
 
 from .blueprint import web_bp
 
 logger = logging.getLogger(__name__)
 
 
-class HomePageView(MethodView):
-    def get(self):
-        result = {"ping": "pong"}
-        return jsonify(result)
+class RenderTemplateView(View):
+    """Template View
+
+    Pulled straight from Flask docs:
+    - https://flask.palletsprojects.com/en/1.1.x/views/
+    """
+
+    def __init__(self, template_name):
+        self.template_name = template_name
+
+    def dispatch_request(self):
+        return render_template(self.template_name)
 
 
-web_bp.add_url_rule("/", view_func=HomePageView.as_view("home"))
+web_bp.add_url_rule(
+    "/", view_func=RenderTemplateView.as_view("home", template_name="index.html")
+)
