09ce4faebc6e9bb3e5b15093710504be4145ad6e
Aly Sivji <alysivji@gmail.com>
2020-07-05 22:22:27 -0500

    test logout view

--- tests/apps/web/test_views.py
+++ tests/apps/web/test_views.py
@@ -15,3 +15,17 @@ def test_login_and_access_restricted_view(login_client, factory):
 
     rv = client.get("/settings", follow_redirects=True)
     assert rv.status_code == 200
+
+
+def test_logout_view(login_client, factory):
+    # Arrange
+    slack_user = factory.SlackUser()
+    client = login_client(user=slack_user)
+    client.get("/logout", follow_redirects=True)
+
+    # Act
+    rv = client.get("/settings", follow_redirects=True)
+
+    # Assert
+    assert rv.status_code == 200
+    assert "slack.com/oauth/v2/authorize" in rv.data.decode("utf-8")
