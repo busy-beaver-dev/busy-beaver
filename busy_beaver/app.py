0a975b595b3cbd0f2093adf20e8e9fa0fd586dbb
Aly Sivji <alysivji@gmail.com>
2020-07-06 16:52:51 -0500

    implement bootstrap

--- busy_beaver/app.py
+++ busy_beaver/app.py
@@ -12,7 +12,7 @@ from .blueprints import (
 from .common.oauth import OAuthError
 from .config import DATABASE_URI, REDIS_URI, SECRET_KEY
 from .exceptions import NotAuthorized, ValidationError
-from .extensions import db, login_manager, migrate, rq, talisman
+from .extensions import bootstrap, db, login_manager, migrate, rq, talisman
 from .toolbox import make_response
 
 
@@ -51,6 +51,7 @@ def create_app(*, testing=False):
     app.config["RQ_QUEUES"] = ["default"]
     rq.init_app(app)
 
+    bootstrap.init_app(app)
     login_manager.init_app(app)
     talisman.init_app(app)
 
--- busy_beaver/extensions.py
+++ busy_beaver/extensions.py
@@ -1,9 +1,11 @@
+from flask_bootstrap import Bootstrap
 from flask_login import LoginManager
 from flask_migrate import Migrate
 from flask_rq2 import RQ
 from flask_sqlalchemy import SQLAlchemy
 from flask_talisman import Talisman
 
+bootstrap = Bootstrap()
 db = SQLAlchemy()
 migrate = Migrate()
 rq = RQ()
