3c27c5f7ff3be8bb658178441fe7421d5c53f651
Aly Sivji <alysivji@gmail.com>
2020-07-28 17:22:15 -0500

    move tests in classes

--- tests/apps/web/test_views.py
+++ tests/apps/web/test_views.py
@@ -1,31 +1,37 @@
+import pytest
+
+
+@pytest.mark.unit
 def test_index(client):
     rv = client.get("/")
     assert rv.status_code == 200
 
 
-def test_access_restricted_view(client):
-    rv = client.get("/settings", follow_redirects=True)
-    assert rv.status_code == 200
-    assert "slack.com/oauth/v2/authorize" in rv.data.decode("utf-8")
+class TestAuthentication:
+    @pytest.mark.unit
+    def test_access_restricted_view(self, client):
+        rv = client.get("/settings", follow_redirects=True)
+        assert rv.status_code == 200
+        assert "slack.com/oauth/v2/authorize" in rv.data.decode("utf-8")
 
+    @pytest.mark.unit
+    def test_login_and_access_restricted_view(self, login_client, factory):
+        slack_user = factory.SlackUser()
+        client = login_client(user=slack_user)
 
-def test_login_and_access_restricted_view(login_client, factory):
-    slack_user = factory.SlackUser()
-    client = login_client(user=slack_user)
+        rv = client.get("/settings", follow_redirects=True)
+        assert rv.status_code == 200
 
-    rv = client.get("/settings", follow_redirects=True)
-    assert rv.status_code == 200
+    @pytest.mark.unit
+    def test_logout_view(self, login_client, factory):
+        # Arrange
+        slack_user = factory.SlackUser()
+        client = login_client(user=slack_user)
+        client.get("/logout", follow_redirects=True)
 
+        # Act
+        rv = client.get("/settings", follow_redirects=True)
 
-def test_logout_view(login_client, factory):
-    # Arrange
-    slack_user = factory.SlackUser()
-    client = login_client(user=slack_user)
-    client.get("/logout", follow_redirects=True)
-
-    # Act
-    rv = client.get("/settings", follow_redirects=True)
-
-    # Assert
-    assert rv.status_code == 200
-    assert "slack.com/oauth/v2/authorize" in rv.data.decode("utf-8")
+        # Assert
+        assert rv.status_code == 200
+        assert "slack.com/oauth/v2/authorize" in rv.data.decode("utf-8")
