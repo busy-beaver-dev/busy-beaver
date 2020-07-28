c14d3de3f2fc0c88da2b4b5a377dc6dcb535ef0e
Aly Sivji <alysivji@gmail.com>
2020-07-28 18:45:55 -0500

    add more tests

--- tests/apps/web/test_views.py
+++ tests/apps/web/test_views.py
@@ -1,6 +1,6 @@
 import pytest
 
-from busy_beaver.models import UpcomingEventsConfiguration
+from busy_beaver.models import UpcomingEventsConfiguration, UpcomingEventsGroup
 from tests._utilities import FakeSlackClient
 
 MODULE_TO_TEST = "busy_beaver.apps.web.views"
@@ -67,7 +67,9 @@ class TestUpcomingEventsViews:
         assert rv.status_code == 200
 
     @pytest.mark.end2end
-    def test_upcoming_events_add_new_group(self, login_client, factory, patch_slack):
+    def test_upcoming_events_view_add_new_group_page(
+        self, login_client, factory, patch_slack
+    ):
         # Arrange
         slack_user = factory.SlackUser()
         client = login_client(user=slack_user)
@@ -80,23 +82,93 @@ class TestUpcomingEventsViews:
         assert rv.status_code == 200
 
     @pytest.mark.end2end
-    @pytest.mark.current
+    @pytest.mark.vcr
+    def test_upcoming_events_add_new_group__success(
+        self, login_client, factory, patch_slack
+    ):
+        # Arrange
+        config = factory.UpcomingEventsConfiguration(enabled=True)
+        slack_user = factory.SlackUser(installation=config.slack_installation)
+        client = login_client(user=slack_user)
+        patch_slack(is_admin=True)
+
+        # Act
+        rv = client.post(
+            "/settings/upcoming-events/group",
+            data={"meetup_urlname": "_CHIPY_"},
+            follow_redirects=True,
+        )
+
+        # Assert
+        assert rv.status_code == 200
+        groups = UpcomingEventsGroup.query.all()
+        assert len(groups) == 1
+        assert groups[0].meetup_urlname == "_ChiPy_"
+
+    @pytest.mark.end2end
+    @pytest.mark.vcr
+    def test_upcoming_events_add_new_group__does_not_exist(
+        self, login_client, factory, patch_slack
+    ):
+        # Arrange
+        config = factory.UpcomingEventsConfiguration(enabled=True)
+        slack_user = factory.SlackUser(installation=config.slack_installation)
+        client = login_client(user=slack_user)
+        patch_slack(is_admin=True)
+
+        # Act
+        rv = client.post(
+            "/settings/upcoming-events/group",
+            data={"meetup_urlname": "adsfasdfeum3n4x"},
+            follow_redirects=True,
+        )
+
+        # Assert
+        assert rv.status_code == 200
+        assert b"Group does not exist" in rv.data
+        groups = UpcomingEventsGroup.query.all()
+        assert len(groups) == 0
+
+    @pytest.mark.end2end
+    @pytest.mark.vcr
+    def test_upcoming_events_add_new_group__group_already_added(
+        self, login_client, factory, patch_slack
+    ):
+        # Arrange
+        config = factory.UpcomingEventsConfiguration(enabled=True)
+        UpcomingEventsGroup(meetup_urlname="_ChiPy_", configuration=config)
+        slack_user = factory.SlackUser(installation=config.slack_installation)
+        client = login_client(user=slack_user)
+        patch_slack(is_admin=True)
+
+        # Act
+        rv = client.post(
+            "/settings/upcoming-events/group",
+            data={"meetup_urlname": "_ChiPy_"},
+            follow_redirects=True,
+        )
+
+        # Assert
+        assert rv.status_code == 200
+        assert b"Group already added" in rv.data
+        groups = UpcomingEventsGroup.query.all()
+        assert len(groups) == 1
+
+    @pytest.mark.end2end
     def test_upcoming_events_toggle_enabled_status(
         self, login_client, factory, patch_slack
     ):
         # Arrange
-        upcoming_events_config = factory.UpcomingEventsConfiguration(enabled=True)
-        slack_user = factory.SlackUser(
-            installation=upcoming_events_config.slack_installation
-        )
+        config = factory.UpcomingEventsConfiguration(enabled=True)
+        slack_user = factory.SlackUser(installation=config.slack_installation)
         client = login_client(user=slack_user)
         patch_slack(is_admin=True)
 
-        # Act)
+        # Act
         rv = client.get("/settings/upcoming-events/toggle", follow_redirects=True)
 
         # Assert
         assert rv.status_code == 200
 
-        config = UpcomingEventsConfiguration.query.get(upcoming_events_config.id)
+        config = UpcomingEventsConfiguration.query.get(config.id)
         assert config.enabled is False
