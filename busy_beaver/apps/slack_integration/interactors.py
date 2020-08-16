b815bb19059ddb11c2df895dac5f8abb084162d9
Aly Sivji <alysivji@gmail.com>
2020-08-15 21:17:36 -0500

    clean up text

--- busy_beaver/apps/slack_integration/interactors.py
+++ busy_beaver/apps/slack_integration/interactors.py
@@ -13,21 +13,28 @@ def make_slack_response(
 
 
 def generate_help_text(installation: SlackInstallation):
-    help_text = (
-        "Busy Beaver is a Community Engagement bot.\n"
-        "\n"
-        "Some commands I understand:\n"
-    )
+    help_text = "Busy Beaver is a Community Engagement bot.\n\n"
+    github_summary_config = installation.github_summary_config
+    upcoming_events_config = installation.upcoming_events_config
 
-    if installation.upcoming_events_config:
-        if installation.upcoming_events_config.enabled:
+    if github_summary_config:
+        if github_summary_config.enabled:
+            help_text += (
+                f"See what projects other members of your community "
+                f"are working on in <#{github_summary_config.channel}>.\n\n"
+            )
+
+    help_text += "Some commands I understand:\n"
+
+    if upcoming_events_config:
+        if upcoming_events_config.enabled:
             help_text += (
                 "`/busybeaver next`: Retrieve next event\n"
                 "`/busybeaver events`: Retrieve list of upcoming event\n"
             )
 
-    if installation.github_summary_config:
-        if installation.github_summary_config.enabled:
+    if github_summary_config:
+        if github_summary_config.enabled:
             help_text += (
                 "`/busybeaver connect`: Connect GitHub Account\n"
                 "`/busybeaver reconnect`: Connect to difference GitHub Account\n"
