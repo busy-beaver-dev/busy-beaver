3c8ad19b3c93821bc96a746a95e988292313a503
Aly Sivji <alysivji@gmail.com>
2020-10-01 16:34:30 -0500

    move test functions into class

--- tests/apps/slack_integration/api/test_event_subscription.py
+++ tests/apps/slack_integration/api/test_event_subscription.py
@@ -42,7 +42,7 @@ def test_slack_callback_url_verification(
     assert resp.json == {"challenge": challenge_code}
 
 
-class TestEventCallbackForDMs:
+class TestDMEventCallback:
     @pytest.mark.integration
     def test_slack_callback_bot_message_is_ignored(
         self, client, session, patch_slack, create_slack_headers
@@ -54,7 +54,7 @@ class TestEventCallbackForDMs:
         )
         data = {
             "type": "event_callback",
-            "event": {"type": "message", "subtype": "bot_message"},
+            "event": {"type": "message", "subtype": "bot_message", "user": "my_id"},
         }
         headers = create_slack_headers(100_000_000, data)
 
@@ -102,44 +102,47 @@ class TestEventCallbackForDMs:
         assert kwargs["channel"] == channel
 
 
-@pytest.mark.end2end
-def test_user_joins_github_summary_channel_with_feature_enabled(
-    client, session, factory, patch_slack, create_slack_headers
-):
-    # Arrange
-    patched_slack = patch_slack("busy_beaver.apps.slack_integration.event_subscription")
-    # Create installation in database
-    workspace_id = "TXXXXXXXXX"
-    authorizing_user_id = "alysivji"
-    bot_id = "test_bot"
-    channel = "busy-beaver"
-    installation = factory.SlackInstallation(
-        authorizing_user_id=authorizing_user_id,
-        workspace_id=workspace_id,
-        workspace_name="Test",
-        bot_user_id=bot_id,
-    )
-    factory.GitHubSummaryConfiguration(
-        enabled=True, channel=channel, slack_installation=installation
-    )
+class TestMemberJoinedChannelEventCallback:
+    @pytest.mark.end2end
+    def test_user_joins_github_summary_channel_with_feature_enabled(
+        self, client, session, factory, patch_slack, create_slack_headers
+    ):
+        # Arrange
+        patched_slack = patch_slack(
+            "busy_beaver.apps.slack_integration.event_subscription"
+        )
+        # Create installation in database
+        workspace_id = "TXXXXXXXXX"
+        authorizing_user_id = "alysivji"
+        bot_id = "test_bot"
+        channel = "busy-beaver"
+        installation = factory.SlackInstallation(
+            authorizing_user_id=authorizing_user_id,
+            workspace_id=workspace_id,
+            workspace_name="Test",
+            bot_user_id=bot_id,
+        )
+        factory.GitHubSummaryConfiguration(
+            enabled=True, channel=channel, slack_installation=installation
+        )
 
-    # Act -- event_subscription callback
-    data = {
-        "type": "event_callback",
-        "team_id": workspace_id,
-        "event": {
-            "type": "member_joined_channel",
-            "user": authorizing_user_id,
-            "channel": channel,
-        },
-    }
-    headers = create_slack_headers(100_000_000, data)
-    client.post("/slack/event-subscription", headers=headers, json=data)
+        # Act -- event_subscription callback
+        data = {
+            "type": "event_callback",
+            "team_id": workspace_id,
+            "event": {
+                "type": "member_joined_channel",
+                "user": authorizing_user_id,
+                "channel": channel,
+            },
+        }
+        headers = create_slack_headers(100_000_000, data)
+        client.post("/slack/event-subscription", headers=headers, json=data)
 
-    # Assert -- check that we send ephhermal message
-    args, kwargs = patched_slack.mock.call_args
-    assert "/busybeaver connect" in args[0]
-    assert kwargs["user_id"] == authorizing_user_id
+        # Assert -- check that we send ephhermal message
+        args, kwargs = patched_slack.mock.call_args
+        assert "/busybeaver connect" in args[0]
+        assert kwargs["user_id"] == authorizing_user_id
 
 
 @pytest.mark.end2end
@@ -180,86 +183,91 @@ def test_user_joins_github_summary_channel_with_feature_disabled(
     assert patched_slack.mock.call_count == 0
 
 
-@pytest.mark.unit
-def test_user_opens_app_home_for_first_time__shown_app_home(
-    client, session, factory, patch_slack, create_slack_headers
-):
-    # Arrange
-    workspace_id = "TXXXXXXXXX"
-    user_id = "U5FTQ3QRZ"
-    installation = factory.SlackInstallation(workspace_id=workspace_id)
-    patched_slack = patch_slack("busy_beaver.apps.slack_integration.event_subscription")
+class TestAppHomeOpenedEventCallback:
+    @pytest.mark.unit
+    def test_user_opens_app_home_for_first_time__shown_app_home(
+        self, client, session, factory, patch_slack, create_slack_headers
+    ):
+        # Arrange
+        workspace_id = "TXXXXXXXXX"
+        user_id = "U5FTQ3QRZ"
+        installation = factory.SlackInstallation(workspace_id=workspace_id)
+        patched_slack = patch_slack(
+            "busy_beaver.apps.slack_integration.event_subscription"
+        )
 
-    # Act
-    data = {
-        "type": "event_callback",
-        "team_id": workspace_id,
-        "event": {"type": "app_home_opened", "user": user_id, "tab": "home"},
-    }
-    app_home_handler(data)
+        # Act
+        data = {
+            "type": "event_callback",
+            "team_id": workspace_id,
+            "event": {"type": "app_home_opened", "user": user_id, "tab": "home"},
+        }
+        app_home_handler(data)
 
-    # Assert -- check we send the app home send
-    args, kwargs = patched_slack.mock.call_args
-    assert args[0] == user_id
-    assert kwargs.get("view", {}) == AppHome().to_dict()
+        # Assert -- check we send the app home send
+        args, kwargs = patched_slack.mock.call_args
+        assert args[0] == user_id
+        assert kwargs.get("view", {}) == AppHome().to_dict()
 
-    params = {"installation_id": installation.id, "slack_id": user_id}
-    user = SlackUser.query.filter_by(**params).first()
-    assert user
-    assert user.app_home_opened_count == 1
+        params = {"installation_id": installation.id, "slack_id": user_id}
+        user = SlackUser.query.filter_by(**params).first()
+        assert user
+        assert user.app_home_opened_count == 1
 
+    @pytest.mark.unit
+    def test_user_opens_app_home_for_greater_than_first_time__shown_app_home(
+        self, client, session, factory, patch_slack, create_slack_headers
+    ):
+        # Arrange
+        workspace_id = "TXXXXXXXXX"
+        user_id = "U5FTQ3QRZ"
+        installation = factory.SlackInstallation(workspace_id=workspace_id)
+        user = factory.SlackUser(slack_id=user_id, installation=installation)
+        original_user_count = user.app_home_opened_count
+        patched_slack = patch_slack(
+            "busy_beaver.apps.slack_integration.event_subscription"
+        )
 
-@pytest.mark.unit
-def test_user_opens_app_home_for_greater_than_first_time__shown_app_home(
-    client, session, factory, patch_slack, create_slack_headers
-):
-    # Arrange
-    workspace_id = "TXXXXXXXXX"
-    user_id = "U5FTQ3QRZ"
-    installation = factory.SlackInstallation(workspace_id=workspace_id)
-    user = factory.SlackUser(slack_id=user_id, installation=installation)
-    original_user_count = user.app_home_opened_count
-    patched_slack = patch_slack("busy_beaver.apps.slack_integration.event_subscription")
+        # Act
+        data = {
+            "type": "event_callback",
+            "team_id": workspace_id,
+            "event": {"type": "app_home_opened", "user": user_id, "tab": "home"},
+        }
+        app_home_handler(data)
 
-    # Act
-    data = {
-        "type": "event_callback",
-        "team_id": workspace_id,
-        "event": {"type": "app_home_opened", "user": user_id, "tab": "home"},
-    }
-    app_home_handler(data)
+        # Assert -- check we send the app home send
+        args, kwargs = patched_slack.mock.call_args
+        assert args[0] == user_id
+        assert kwargs.get("view", {}) == AppHome().to_dict()
 
-    # Assert -- check we send the app home send
-    args, kwargs = patched_slack.mock.call_args
-    assert args[0] == user_id
-    assert kwargs.get("view", {}) == AppHome().to_dict()
+        params = {"installation_id": installation.id, "slack_id": user_id}
+        user = SlackUser.query.filter_by(**params).first()
+        assert user
+        assert user.app_home_opened_count == original_user_count + 1
 
-    params = {"installation_id": installation.id, "slack_id": user_id}
-    user = SlackUser.query.filter_by(**params).first()
-    assert user
-    assert user.app_home_opened_count == original_user_count + 1
+    @pytest.mark.unit
+    def test_user_opens_app_home_message_tab__does_nothing(
+        self, client, session, factory, patch_slack, create_slack_headers
+    ):
+        # Arrange
+        workspace_id = "TXXXXXXXXX"
+        user_id = "U5FTQ3QRZ"
+        factory.SlackInstallation(workspace_id=workspace_id)
+        patched_slack = patch_slack(
+            "busy_beaver.apps.slack_integration.event_subscription"
+        )
 
+        # Act
+        data = {
+            "type": "event_callback",
+            "team_id": workspace_id,
+            "event": {"type": "app_home_opened", "user": user_id, "tab": "messages"},
+        }
+        app_home_handler(data)
 
-@pytest.mark.unit
-def test_user_opens_app_home_message_tab__does_nothing(
-    client, session, factory, patch_slack, create_slack_headers
-):
-    # Arrange
-    workspace_id = "TXXXXXXXXX"
-    user_id = "U5FTQ3QRZ"
-    factory.SlackInstallation(workspace_id=workspace_id)
-    patched_slack = patch_slack("busy_beaver.apps.slack_integration.event_subscription")
-
-    # Act
-    data = {
-        "type": "event_callback",
-        "team_id": workspace_id,
-        "event": {"type": "app_home_opened", "user": user_id, "tab": "messages"},
-    }
-    app_home_handler(data)
-
-    # Assert
-    assert patched_slack.mock.assert_not_called
+        # Assert
+        assert patched_slack.mock.assert_not_called
 
 
 class TestAppUninstalledEventCallback:
