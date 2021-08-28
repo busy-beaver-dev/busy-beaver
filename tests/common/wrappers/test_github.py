48136c72941cb067ef8c90a7642f6206fb7b3077
Aly Sivji <alysivji@gmail.com>
2021-08-28 09:43:31 -0500

    move tests into class

--- tests/common/wrappers/test_github.py
+++ tests/common/wrappers/test_github.py
@@ -13,53 +13,52 @@ def client():
     yield GitHubClient(oauth_token=GITHUB_OAUTH_TOKEN)
 
 
-@pytest.mark.vcr()
-def test_all_user_stars(client):
-    starred_repos = client.all_user_stars(user="alysivji", max_pages=4)
+class TestGitHubClient:
+    @pytest.mark.vcr()
+    def test_all_user_stars(self, client):
+        starred_repos = client.all_user_stars(user="alysivji", max_pages=4)
 
-    assert len(starred_repos) == 120
+        assert len(starred_repos) == 120
 
+    @pytest.mark.vcr()
+    def test_all_user_activity_after(self, client):
+        # TODO don't like how I hardcoded date, think of more robust solution
+        vrcpy_timestamp = pytz.utc.localize(datetime(2018, 11, 24, 22, 55, 3, 767_521))
+        boundary_dt = vrcpy_timestamp - timedelta(days=1)
+        user_activity = client.user_activity_after(
+            user="alysivji", timestamp=boundary_dt
+        )
 
-@pytest.mark.vcr()
-def test_all_user_activity_after(client):
-    # TODO don't like how I hardcoded date, think of more robust solution
-    vrcpy_timestamp = pytz.utc.localize(datetime(2018, 11, 24, 22, 55, 3, 767_521))
-    boundary_dt = vrcpy_timestamp - timedelta(days=1)
-    user_activity = client.user_activity_after(user="alysivji", timestamp=boundary_dt)
+        assert len(user_activity) >= 0
 
-    assert len(user_activity) >= 0
+    @pytest.mark.vcr()
+    @pytest.mark.freeze_time("2020-01-13")
+    def test_user_activity_during_range(self, client):
+        # Arrange
+        start_dt = pytz.utc.localize(datetime.utcnow())
+        end_dt = start_dt + timedelta(days=1)
 
+        # Act
+        user_activity = client.user_activity_during_range(
+            user="alysivji", start_dt=start_dt, end_dt=end_dt
+        )
 
-@pytest.mark.vcr()
-@pytest.mark.freeze_time("2020-01-13")
-def test_user_activity_during_range(client):
-    # Arrange
-    start_dt = pytz.utc.localize(datetime.utcnow())
-    end_dt = start_dt + timedelta(days=1)
+        # Assert
+        assert len(user_activity) >= 0
+        for event in user_activity:
+            assert start_dt <= parse_dt(event["created_at"]) <= end_dt
 
-    # Act
-    user_activity = client.user_activity_during_range(
-        user="alysivji", start_dt=start_dt, end_dt=end_dt
-    )
+    @pytest.mark.vcr()
+    def test_all_user_repos(self, client):
+        all_repos = client.all_user_repos("alysivji")
 
-    # Assert
-    assert len(user_activity) >= 0
-    for event in user_activity:
-        assert start_dt <= parse_dt(event["created_at"]) <= end_dt
+        assert len(all_repos) >= 50
 
+    @pytest.mark.vcr()
+    def test_user_details(self, client):
+        details = client.user_details()
 
-@pytest.mark.vcr()
-def test_all_user_repos(client):
-    all_repos = client.all_user_repos("alysivji")
-
-    assert len(all_repos) >= 50
-
-
-@pytest.mark.vcr()
-def test_user_details(client):
-    details = client.user_details()
-
-    assert details["login"] == "alysivji"
+        assert details["login"] == "alysivji"
 
 
 @pytest.mark.parametrize(
